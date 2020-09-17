fee4a4d4e314892ee73671007220d939d505510e
Sunil Mair <sunil.r.mair@gmail.com>
2020-09-17 21:36:05 +0000

    Ag py script working

--- lab1_samples/Ag_lat_eam.py
+++ lab1_samples/Ag_lat_eam.py
@@ -15,7 +15,7 @@ read_data $DATAINPUT
 
 # ---------- 2. Specify interatomic potential ---------------------
 pair_style eam
-pair_coeff * * $POTENTIAL  Ag
+pair_coeff * * $POTENTIAL
 
 # pair_style lj/cut 4.5
 # pair_coeff 1 1 0.3450 2.644 4.5
