bf6a5e6d927a2eb2c3421ea831b27557b98ea4d2
Sunil Mair <sunil.r.mair@gmail.com>
2020-09-17 21:44:01 -0400

    Added vacancy energy functions to Ag py file

--- lab1_samples/Ag_lat_eam.py
+++ lab1_samples/Ag_lat_eam.py
@@ -22,14 +22,14 @@ pair_coeff * * $POTENTIAL
 
 # ---------- 3. Run single point calculation  ---------------------
 thermo_style custom step pe lx ly lz press pxx pyy pzz
-#run 0
+run 0
 
 #-- include optimization of the unit cell parameter
-fix 1 all box/relax iso 0.0 vmax 0.001
+#fix 1 all box/relax iso 0.0 vmax 0.001
 
 #-- enable optimization of atomic positions (and the cell)
-min_style cg
-minimize 1e-10 1e-10 1000 10000
+#min_style cg
+#minimize 1e-10 1e-10 1000 10000
 
 
 # ---- 4. Define and print useful variables -------------
@@ -42,15 +42,32 @@ print "Number of atoms = ${natoms}"
 print "Lattice constant (Angstoms) = ${length}"
         """
 
-def make_struc(alat):
+def make_struc(alat, supercell_size):
     """
     Creates the crystal structure using ASE.
     :param alat: Lattice parameter in angstrom
     :return: structure object converted from ase
     """
     unitcell = crystal('Ag', [(0, 0, 0)], spacegroup=225, cellpar=[alat, alat, alat, 90, 90, 90])
-    multiplier = numpy.identity(3) * 2
+    multiplier = numpy.identity(3) * supercell_size
     ase_supercell = make_supercell(unitcell, multiplier)
+    structure = Struc(ase2struc(ase_supercell))
+    return structure
+
+def make_struc_vac(alat, supercell_size):
+    """
+    Creates the fcc crystal structure with central atom missing using ASE.
+    :param alat: Lattice parameter in angstrom
+    :param supercell_size: supercell multiplier
+    :return: structure object converted from ase
+    """
+    unitcell = crystal('Ag', [(0, 0, 0)], spacegroup=225, cellpar=[alat, alat, alat, 90, 90, 90])
+    multiplier = numpy.identity(3) * supercell_size
+    ase_supercell = make_supercell(unitcell, multiplier)
+    for i in range(len(ase_supercell.positions)):
+        if (ase_supercell.positions[i] == [supercell_size*alat/2, supercell_size*alat/2, supercell_size*alat/2]).all():
+            ase_supercell.pop(i)
+            break
     structure = Struc(ase2struc(ase_supercell))
     return structure
 
@@ -67,6 +84,27 @@ def compute_energy(alat, template):
     energy, lattice = get_lammps_energy(outfile=output_file)
     return energy, lattice
 
+def compute_vac_energy(alat, supercell_size, template):
+    """
+    Make an input template and select potential and structure, and the path where to run
+    Computes energy of supercell with and without vacancy
+    """
+    potpath = os.path.join(os.environ['LAMMPS_POTENTIALS'],'Ag_u3.eam')
+    potential = ClassicalPotential(path=potpath, ptype='eam', element=["Ag"])
+    runpath = Dir(path=os.path.join(os.environ['WORKDIR'], "Lab1", str(alat)))
+
+    struc = make_struc(alat=alat, supercell_size=supercell_size)
+    output_file = lammps_run(struc=struc, runpath=runpath, potential=potential, intemplate=template, inparam={})
+    energy, lattice = get_lammps_energy(outfile=output_file)
+
+    struc_vac = make_struc_vac(alat=alat, supercell_size=supercell_size)
+    output_file = lampmps_run(struc=struc, runpath=runpath, potential=potential, intemplate=template, inparam={})
+    energy_vac, lattice_vac = get_lammps_energy(outfile=output_file)
+
+    print(f'Energy: {energy}')
+    print(f'Energy with vacancy: {energy_vac}')
+    return energy, energy_vac
+
 
 def lattice_scan():
     alat_list = numpy.linspace(3.8, 4.3, 6)
@@ -80,4 +118,5 @@ def lattice_scan():
 if __name__ == '__main__':
     # put here the function that you actually want to run
     #lattice_scan()
-    compute_energy(4.1, input_template)
+    #compute_energy(4.1, input_template)
+    compute_vac_energy(4.08999994375577, input_template)
